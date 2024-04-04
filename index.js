23cb0483521e0356755ee5cc7841a0abf6d73fa0
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 20:44:46 +0500

    Task no 19

---
+++ index.js
@@ -0,0 +1,6 @@
+/// Task # 19 //
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+//print a message indicating the number of people you are inviting to dinner.
+let guest_dinner = ["Kinza", "Rafia", "Kashaf", "Tuba", "Fariha"];
+console.log(`I invited ${guest_dinner.length} people to dinner.`);
+export {};
